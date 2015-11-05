0345969ceeeb6b3f1253030ad4936b185ebe424d
ruoxijing <rjing@hawk.iit.edu>
2015-11-05 10:09:07 -0600

    Modified

--- gallery.php
+++ gallery.php
@@ -11,12 +11,13 @@ require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
+#'version'=>'latest',
 'region'  => 'us-west-2'
 ));
 
-$result = $client->describeDBInstances(array(
-    'DBInstanceIdentifier' => 'itmo544jrxdb',
-));
+#$result = $client->describeDBInstances(array(
+#    'DBInstanceIdentifier' => 'itmo544jrxdb',
+#));
 
 #$endpoint = "";
 
