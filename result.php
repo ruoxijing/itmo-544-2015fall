89f9b1ba4d667ce81346db8a3052e4ed4ec00e24
ruoxijing <rjing@hawk.iit.edu>
2015-11-08 10:57:54 -0600

    Modified sqlconnect

--- result.php
+++ result.php
@@ -1,129 +1,80 @@
 <?php
-// Start the session
+//start the session
 session_start();
-// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
-// of $_FILES.
-$useremail = $_POST["useremail"];
-echo $useremail;
-$uploaddir = '/tmp/';
+//echo $_FILES;
+echo $_POST['useremail'];
+echo $_POST['phone'];
+$uploaddir='/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
+if(move_uploaded_file($_FILES['userfile']['tmp_name'],$uploadfile)){
+	echo "File is calid, and was successfully uploaded.\n";
+}else{
+	echo "Possible file upload attack!\n";
 }
 echo 'Here is some more debugging info:';
 print_r($_FILES);
 print "</pre>";
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
-
 $client = S3Client::factory(array(
 'version' =>'latest',
 'region'  => 'us-west-2'
 ));
-
-#$s3 = new Aws\S3\S3Client([
-#    'version' => 'latest',
-#    'region'  => 'us-west-2'
-#]);
-
-$bucket = uniqid("php-jrx-",false);
+$bucket = uniqid("php-jrh-",false);
 $result = $client->createBucket(array(
-    'Bucket' => $bucket
+	'Bucket'=> $bucket
 ));
-# AWS PHP SDK version 3 create bucket
-#$result = $s3->createBucket([
-#    'ACL' => 'public-read',
-#    'Bucket' => $bucket
-#]);
 $client->waitUntil('BucketExists', array('Bucket' => $bucket));
-#$client->waitUntilBucketExists(array('Bucket' => $bucket));
-#Old PHP SDK version 2
+//$client->waitUntilBucketExists(array('Bucket' => $bucket));
 $key = $uploadfile;
-$result = $s3->putObject(array(
-    'ACL' => 'public-read',
-    'Bucket' => $bucket,
-    'Key' => $key,
-    'SourceFile' => $uploadfile 
+$result = $client->putObject(array(
+	'ACL'=>'public-read',
+	'Bucket'=>$bucket,
+	'Key'=>$key,
+	'SourceFile'=> $uploadfile
 ));
-# PHP version 3
-#$result = $s3->putObject([
-#    'ACL' => 'public-read',
-#    'Bucket' => $bucket,
-#   'Key' => $uploadfile
-#]);  
-
 $url = $result['ObjectURL'];
 echo $url;
-
-$rds = new Aws\Rds\RdsClient([
-    'version' => 'latest',
-    'region'  => 'us-west-2'
-]);
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
 'version' =>'latest',	
 'region' => 'us-west-2'
 ));
 
-#$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'jrx-db'
-#]);
-
-$result = $rds->describeDBInstances([
-   'DBInstanceIdentifier' => 'jrx-db'
-    #'Filters' => [
-    #    [
-    #        'Name' => '<string>', // REQUIRED
-    #        'Values' => ['<string>', ...], // REQUIRED
-    #    ],
-        // ...
-   # ],
-   # 'Marker' => '<string>',
-   # 'MaxRecords' => <integer>,
-]);
-print_r($result);
-$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-    echo "============\n". $endpoint . "================";
-
-print_r($endpoint);  
-//echo "begin database";^M
-#$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link));
-#$link = mysqli_connect("jrxdb.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","jrx-db",3306) or die("Error " . mysqli_error($link));
-$link = mysqli_connect($endpoint,"rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
+$link = mysqli_connect("jrx-db.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/* Prepared statement, stage 1: prepare */
-if (!($stmt = $link->prepare("INSERT INTO items (id, email,phone,filename,s3rawurl,s3finishedurl,status,issubscribed) VALUES (NULL,?,?,?,?,?,?,?)"))) {
-    echo "Prepare failed: (" . $link->errno . ") " . $link->error;
+//check connection
+//if (mysqli_connect_error()){
+//	printf("Connect failed: %s\n",mysqli_connect_error());
+//	exit();0
+//}
+//prepared statement, stage1:prepare
+if(!($stmt =$link->prepare("INSERT INTO items (id,email,phone,filename,s3rawurl,s3finishedurl,status,issubscribed)VALUES(NULL,?,?,?,?,?,?,?)"))){
+	echo "Prepare failed:(" . $link->errno . ")" . $link->error;
 }
 $email = $_POST['useremail'];
 $phone = $_POST['phone'];
-$s3rawurl = $result['ObjectURL']; //$url; from above
+$s3rawurl = $result['ObjectURL'];//;from above
 $filename = basename($_FILES['userfile']['name']);
 $s3finishedurl = "none";
-$status =0;
+$status=0;
 $issubscribed=0;
 $stmt->bind_param("sssssii",$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$issubscribed);
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+if(!$stmt->execute()){
+	echo "Execute failed:(" . $stmt->errno . ")" . $stmt->error;
 }
 printf("%d Row inserted.\n", $stmt->affected_rows);
-/* explicit close recommended */
 $stmt->close();
 $link->real_query("SELECT * FROM items");
 $res = $link->use_result();
 echo "Result set order...\n";
-while ($row = $res->fetch_assoc()) {
-    echo $row['id'] . " " .$row['email']. " " .$row['phone'];
+while ($row = $res->fetch_assoc()){
+	echo $row['id'] . " " .$row['email']. " " .$row['phone'];
 }
 $link->close();
-//add code to detect if subscribed to SNS topic 
-//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
-// add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
-//  Add code to update database to UPDATE status column to 1 (in progress)
 ?>
