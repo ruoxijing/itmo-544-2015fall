27d2965154fe70efbca71c09c20feab2db56f4e7
ruoxijing <rjing@hawk.iit.edu>
2015-11-08 23:52:22 -0600

    Modified connect

--- result.php
+++ result.php
@@ -86,7 +86,7 @@ $client = RdsClient::factory(array(
 #print_r($endpoint);  
 //echo "begin database";^M
 #$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link));
-$link = mysqli_connect("jrx-db.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
+$link = mysqli_connect("jrx-db.cwom1zatgb1y.us-west-2.rds.amazonaws.com","rjing","mypoorphp","itmo544mp1");// or die("Error " . mysqli_error($link));
 #$link = mysqli_connect($endpoint,"rjing","mypoorphp","itmo544mp1") or die("Error " . mysqli_error($link));
 /* check connection */
 if (mysqli_connect_errno()) {
